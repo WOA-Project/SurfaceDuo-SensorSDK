9f4705d92697199b6f205fa5317aa39ed32f6830
Gustave Monce <gustave.monce@outlook.com>
2024-11-01 15:48:17 +0100

    Initial Commit

---
+++ rebuild.cmd
@@ -0,0 +1,3 @@
+cd Windows.Internal.Devices.Sensors
+midlrt Windows.Internal.Devices.Sensors.idl /metadata_dir "C:\Program Files (x86)\Windows Kits\10\References\10.0.22621.0\Windows.Foundation.FoundationContract\4.0.0.0"
+cd ..
